a9e9766daec3cd689af696202825c4078d0da0a8
TomatoDev <diredarknil@gmail.com>
2017-02-14 15:13:36 -0600

    implement sharedprefs for saving token and color choice, and radio selection settings activity should be done

--- app/src/main/java/com/tomatogaming/ttt/SettingsActivity.java
+++ app/src/main/java/com/tomatogaming/ttt/SettingsActivity.java
@@ -1,17 +1,21 @@
 package com.tomatogaming.ttt;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity
 {
-
-    String token = "x";
-    String color = "red";
+    String token;
+    String color;
+    int checkXO;
+    int checkColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,7 +25,21 @@ public class SettingsActivity extends AppCompatActivity
 
         Button btnBack = (Button)findViewById(R.id.btnBack);
         RadioGroup radioXO = (RadioGroup)findViewById(R.id.radioGroupX);
-        RadioGroup radioColor = (RadioGroup)findViewById(R.id.radioGroup2);
+        RadioGroup radioColor = (RadioGroup)findViewById(R.id.radioGroupColor);
+        final ImageView selectedToken = (ImageView)findViewById(R.id.imgSelectedToken) ;
+
+        //load saved prefs if saved
+        SharedPreferences settings = getSharedPreferences("prefs", 0);
+        token = settings.getString("token", "x");
+        color = settings.getString("color", "red");
+        checkXO = settings.getInt("checkXO", R.id.radioX);
+        checkColor = settings.getInt("checkColor", R.id.radioRed);
+
+        //set the defaults to the loaded values
+        radioXO.check(checkXO);
+        radioColor.check(checkColor);
+        int marker = getResources().getIdentifier(token+"_"+color, "drawable", getPackageName());
+        selectedToken.setImageResource(marker);
 
         //when the radio in XO is changed
         radioXO.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
@@ -29,6 +47,7 @@ public class SettingsActivity extends AppCompatActivity
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i)
             {
+                checkXO = i;
                 switch (i)
                 {
                     case R.id.radioX:
@@ -39,6 +58,9 @@ public class SettingsActivity extends AppCompatActivity
                         token = "o";
                         break;
                 }
+                //update the token
+                int marker = getResources().getIdentifier(token+"_"+color, "drawable", getPackageName());
+                selectedToken.setImageResource(marker);
             }
         });
 
@@ -48,6 +70,7 @@ public class SettingsActivity extends AppCompatActivity
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i)
             {
+                checkColor = i;
                 switch (i)
                 {
                     case R.id.radioBlue:
@@ -75,6 +98,9 @@ public class SettingsActivity extends AppCompatActivity
                         color = "teal";
                         break;
                 }
+                //update the token
+                int marker = getResources().getIdentifier(token+"_"+color, "drawable", getPackageName());
+                selectedToken.setImageResource(marker);
             }
         });
 
@@ -86,10 +112,20 @@ public class SettingsActivity extends AppCompatActivity
             {
                 //TODO
                 //save settings
+                SharedPreferences settings = getSharedPreferences("prefs", MODE_PRIVATE);
+                SharedPreferences.Editor edit = settings.edit();
+
+                //save the strings
+                edit.putString("token", token);
+                edit.putString("color", color);
+                edit.putInt("checkXO", checkXO);
+                edit.putInt("checkColor", checkColor);
+                edit.commit();
 
                 //back to the main menu
                 Intent menu = new Intent(SettingsActivity.this, MainMenu.class);
                 startActivity(menu);
+                finish();
             }
         });
     }
