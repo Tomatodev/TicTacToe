52894c9d3b716fa3c02a4d91a817bbc7ceed48e5
TomatoDev <diredarknil@gmail.com>
2017-02-16 15:05:59 -0600

    removed backgrounds implemented p1 half of turn logic

--- app/src/main/java/com/tomatogaming/ttt/GameActivity.java
+++ app/src/main/java/com/tomatogaming/ttt/GameActivity.java
@@ -7,9 +7,121 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
 
 public class GameActivity extends AppCompatActivity
 {
+    public boolean isOnline = false; //whether or not the game is online
+    public boolean gameState = true; //if the game is running
+    public boolean turn = true; //turn counter will be from the perspective of player1
+    public boolean[] gridState = {false, false, false, false, false, false, false, false, false}; //if a grid is occupied
+
+
+    public void p2Turn()
+    {
+        if (!isOnline) //if ai
+        {
+            TextView txtTurn = (TextView)findViewById(R.id.txtTurn);
+            Button btnPass = (Button)findViewById(R.id.btnPass);
+            //grid buttons
+            final ImageButton[] grid = new ImageButton[9];
+            grid[0] = (ImageButton)findViewById(R.id.tacGrid0);
+            grid[1] = (ImageButton)findViewById(R.id.tacGrid1);
+            grid[2] = (ImageButton)findViewById(R.id.tacGrid2);
+            grid[3] = (ImageButton)findViewById(R.id.tacGrid3);
+            grid[4] = (ImageButton)findViewById(R.id.tacGrid4);
+            grid[5] = (ImageButton)findViewById(R.id.tacGrid5);
+            grid[6] = (ImageButton)findViewById(R.id.tacGrid6);
+            grid[7] = (ImageButton)findViewById(R.id.tacGrid7);
+            grid[8] = (ImageButton)findViewById(R.id.tacGrid8);
+
+            btnPass.setAlpha(0.5f); //fade the pass button
+            btnPass.setClickable(false); //disable button
+            txtTurn.setText(R.string.uiTheirTurn);
+
+            //loop through the grid buttons and disable them
+            for (int i = 0; i < grid.length; i++)
+            {
+                grid[i].setClickable(false);
+            }
+
+        }
+        else //online match
+        {
+
+        }
+    }
+
+    public void p1turn()
+    {
+        TextView txtTurn = (TextView)findViewById(R.id.txtTurn);
+        Button btnPass = (Button)findViewById(R.id.btnPass);
+        //grid buttons
+        final ImageButton[] grid = new ImageButton[9];
+        grid[0] = (ImageButton)findViewById(R.id.tacGrid0);
+        grid[1] = (ImageButton)findViewById(R.id.tacGrid1);
+        grid[2] = (ImageButton)findViewById(R.id.tacGrid2);
+        grid[3] = (ImageButton)findViewById(R.id.tacGrid3);
+        grid[4] = (ImageButton)findViewById(R.id.tacGrid4);
+        grid[5] = (ImageButton)findViewById(R.id.tacGrid5);
+        grid[6] = (ImageButton)findViewById(R.id.tacGrid6);
+        grid[7] = (ImageButton)findViewById(R.id.tacGrid7);
+        grid[8] = (ImageButton)findViewById(R.id.tacGrid8);
+
+
+        btnPass.setAlpha(1); //enable
+        btnPass.setClickable(true); //enable button
+        txtTurn.setText(R.string.uiYourTurn);
+
+        //loop through the grid buttons and enable them
+        for (int i = 0; i < grid.length; i++)
+        {
+            grid[i].setClickable(true);
+        }
+
+    }
+
+    public int aiWouldWin() //check and see if only one is needed to win
+    {
+        int pick = 0;
+        boolean canWin = false;
+        boolean canBlock = false;
+
+        //TODO
+        //iterate over the grid and see if any are winners
+
+        //if canWin true return winner
+        if (canWin)
+            return pick;
+
+            //if false check for blocks
+        else
+        {
+            //iterate over the grid and see if p1 is about to win
+            //if true return block
+            if (canBlock)
+            {
+                return pick;
+            }
+            //if false pick at random
+            else
+            {
+                //while random !available
+                return 5;
+            }
+        }
+
+
+    }
+
+    public void aiPlacePick(int pick) //put the marker
+    {
+        //TODO
+        //check and see if spot is free
+        //place marker
+        //end turn
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,6 +138,7 @@ public class GameActivity extends AppCompatActivity
         final String color = settings.getString("color", "red");
         final int marker = getResources().getIdentifier(token+"_"+color, "drawable", this.getPackageName());
 
+
         //grid buttons
         final ImageButton[] grid = new ImageButton[9];
         grid[0] = (ImageButton)findViewById(R.id.tacGrid0);
@@ -38,14 +151,28 @@ public class GameActivity extends AppCompatActivity
         grid[7] = (ImageButton)findViewById(R.id.tacGrid7);
         grid[8] = (ImageButton)findViewById(R.id.tacGrid8);
 
+
         //grid presses
         grid[0].setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                grid[0].setClickable(false);
-                grid[0].setImageResource(marker);
+                if (!gridState[0]) //if this grid is not occupied
+                {
+                    if (turn) //if it is your turn
+                    {
+                        grid[0].setClickable(false); //disable future clicks
+                        grid[0].setImageResource(marker); //place your marker
+                        gridState[0] = true; //set this grid to occupied
+                        turn = false; //turn over
+                        p2Turn();
+                    }
+                }
+                else //theres already a token here
+                {
+                    grid[0].setClickable(false); //disable future clicks
+                }
             }
         });
         grid[1].setOnClickListener(new View.OnClickListener()
@@ -53,8 +180,21 @@ public class GameActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                grid[1].setClickable(false);
-                grid[1].setImageResource(marker);
+                if (!gridState[1]) //if this grid is not occupied
+                {
+                    if (turn) //if it is your turn
+                    {
+                        grid[1].setClickable(false); //disable future clicks
+                        grid[1].setImageResource(marker); //place your marker
+                        gridState[1] = true; //set this grid to occupied
+                        turn = false; //turn over
+                        p2Turn();
+                    }
+                }
+                else //theres already a token here
+                {
+                    grid[1].setClickable(false); //disable future clicks
+                }
             }
         });
         grid[2].setOnClickListener(new View.OnClickListener()
@@ -62,8 +202,21 @@ public class GameActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                grid[2].setClickable(false);
-                grid[2].setImageResource(marker);
+                if (!gridState[2]) //if this grid is not occupied
+                {
+                    if (turn) //if it is your turn
+                    {
+                        grid[2].setClickable(false); //disable future clicks
+                        grid[2].setImageResource(marker); //place your marker
+                        gridState[2] = true; //set this grid to occupied
+                        turn = false; //turn over
+                        p2Turn();
+                    }
+                }
+                else //theres already a token here
+                {
+                    grid[2].setClickable(false); //disable future clicks
+                }
             }
         });
         grid[3].setOnClickListener(new View.OnClickListener()
@@ -71,8 +224,21 @@ public class GameActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                grid[3].setClickable(false);
-                grid[3].setImageResource(marker);
+                if (!gridState[3]) //if this grid is not occupied
+                {
+                    if (turn) //if it is your turn
+                    {
+                        grid[3].setClickable(false); //disable future clicks
+                        grid[3].setImageResource(marker); //place your marker
+                        gridState[3] = true; //set this grid to occupied
+                        turn = false; //turn over
+                        p2Turn();
+                    }
+                }
+                else //theres already a token here
+                {
+                    grid[3].setClickable(false); //disable future clicks
+                }
             }
         });
         grid[4].setOnClickListener(new View.OnClickListener()
@@ -80,8 +246,21 @@ public class GameActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                grid[4].setClickable(false);
-                grid[4].setImageResource(marker);
+                if (!gridState[4]) //if this grid is not occupied
+                {
+                    if (turn) //if it is your turn
+                    {
+                        grid[4].setClickable(false); //disable future clicks
+                        grid[4].setImageResource(marker); //place your marker
+                        gridState[4] = true; //set this grid to occupied
+                        turn = false; //turn over
+                        p2Turn();
+                    }
+                }
+                else //theres already a token here
+                {
+                    grid[4].setClickable(false); //disable future clicks
+                }
             }
         });
         grid[5].setOnClickListener(new View.OnClickListener()
@@ -89,8 +268,21 @@ public class GameActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                grid[5].setClickable(false);
-                grid[5].setImageResource(marker);
+                if (!gridState[5]) //if this grid is not occupied
+                {
+                    if (turn) //if it is your turn
+                    {
+                        grid[5].setClickable(false); //disable future clicks
+                        grid[5].setImageResource(marker); //place your marker
+                        gridState[5] = true; //set this grid to occupied
+                        turn = false; //turn over
+                        p2Turn();
+                    }
+                }
+                else //theres already a token here
+                {
+                    grid[5].setClickable(false); //disable future clicks
+                }
             }
         });
         grid[6].setOnClickListener(new View.OnClickListener()
@@ -98,8 +290,21 @@ public class GameActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                grid[6].setClickable(false);
-                grid[6].setImageResource(marker);
+                if (!gridState[6]) //if this grid is not occupied
+                {
+                    if (turn) //if it is your turn
+                    {
+                        grid[6].setClickable(false); //disable future clicks
+                        grid[6].setImageResource(marker); //place your marker
+                        gridState[6] = true; //set this grid to occupied
+                        turn = false; //turn over
+                        p2Turn();
+                    }
+                }
+                else //theres already a token here
+                {
+                    grid[6].setClickable(false); //disable future clicks
+                }
             }
         });
         grid[7].setOnClickListener(new View.OnClickListener()
@@ -107,8 +312,21 @@ public class GameActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                grid[7].setClickable(false);
-                grid[7].setImageResource(marker);
+                if (!gridState[7]) //if this grid is not occupied
+                {
+                    if (turn) //if it is your turn
+                    {
+                        grid[7].setClickable(false); //disable future clicks
+                        grid[7].setImageResource(marker); //place your marker
+                        gridState[7] = true; //set this grid to occupied
+                        turn = false; //turn over
+                        p2Turn();
+                    }
+                }
+                else //theres already a token here
+                {
+                    grid[7].setClickable(false); //disable future clicks
+                }
             }
         });
         grid[8].setOnClickListener(new View.OnClickListener()
@@ -116,12 +334,39 @@ public class GameActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                grid[8].setClickable(false);
-                grid[8].setImageResource(marker);
+                if (!gridState[8]) //if this grid is not occupied
+                {
+                    if (turn) //if it is your turn
+                    {
+                        grid[8].setClickable(false); //disable future clicks
+                        grid[8].setImageResource(marker); //place your marker
+                        gridState[8] = true; //set this grid to occupied
+                        turn = false; //turn over
+                        p2Turn();
+                    }
+                }
+                else //theres already a token here
+                {
+                    grid[8].setClickable(false); //disable future clicks
+                }
             }
         });
 
 
+        //when pass is pressed
+        btnPass.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (turn) //if it is your turn
+                {
+                    turn = false; //turn over
+                    p2Turn();
+                }
+
+            }
+        });
 
         //when the quit button is pressed
         btnQuit.setOnClickListener(new View.OnClickListener()
@@ -141,6 +386,8 @@ public class GameActivity extends AppCompatActivity
                 finish();
             }
         });
+
+        p1turn();
     }
 
 
@@ -151,4 +398,7 @@ public class GameActivity extends AppCompatActivity
 
     }
 
+
+
 }
+
